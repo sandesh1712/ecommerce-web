088290c7041ddd70bac5a9b05a274bb00543288c
= <=>
2023-07-27 21:30:26 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Pages/Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="container">
+         <Login/>
+       </div>
+    </div>
+  );
+}
+
+export default App;
